999ed9b5851df4ef4bce1d6667c430ade370e941
danez13 <hernandezdaniel170@gmail.com>
2024-11-25 09:59:39 -0500

    UPDATE

---
+++ saliency_eval/faithfulness.py
@@ -0,0 +1,96 @@
+"""Computing Faithfulness measure for the saliency scores."""
+import argparse
+import os
+import random
+from argparse import Namespace
+from functools import partial
+
+import numpy as np
+import torch
+from sklearn.metrics import auc
+from transformers import BertTokenizer
+
+from models.train_cnn import eval_model
+from models.data_loader import BucketBatchSampler, DatasetSaliency, collate_threshold, collate_nli, NLIDataset
+from models.model_builder import CNN_MODEL
+
+
+def get_model(model_path):
+    checkpoint = torch.load(model_path,
+                            map_location=lambda storage, loc: storage)
+    model_args = checkpoint['args']
+
+    model_cp = CNN_MODEL(tokenizer, model_args,
+                             n_labels=checkpoint['args']['labels']).to(device)
+
+    model_cp.load_state_dict(checkpoint['model'])
+
+    return model_cp, model_args
+
+
+if __name__ == "__main__":
+    args = {
+        "gpu": False,
+        "saliency": ["shap", "sal_mean", "sal_l2", "occlusion_none", "lime", "inputx_mean", "inputx_l2", "guided_mean", "guided_l2"],
+        "dataset": "snli",
+        "dataset_dir": "data/e-SNLI/dataset",
+        "test_saliency_dir": ["data/saliency/snli/cnn/","data/saliency/snli/random_cnn/"],
+        "model_path": ["data/models/snli/cnn/cnn","data/models/snli/random_cnn/cnn"],
+        "models_dir": ["data/models/snli/cnn/","data/models/snli/random_cnn/"],
+        "model": "cnn"
+    }
+
+    device = torch.device("cuda") if args["gpu"] else torch.device("cpu")
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    thresholds = list(range(0, 110, 10))
+    aucs = []
+
+    coll_call = collate_nli
+
+
+    eval_fn = eval_model
+
+    for saliency in args["saliency"]:
+        for models_dir,test_saliency_dir in zip(args["models_dir"],args["test_saliency_dir"]):
+            for model_path in os.listdir(models_dir):
+                if model_path.endswith('.predictions'):
+                    continue
+                print('Model', model_path, flush=True)
+                model_full_path = os.path.join(models_dir, model_path)
+                model, model_args = get_model(model_full_path)
+
+                random.seed(model_args["seed"])
+                torch.manual_seed(model_args["seed"])
+                torch.cuda.manual_seed_all(model_args["seed"])
+                torch.backends.cudnn.deterministic = True
+                np.random.seed(model_args["seed"])
+
+                model_scores = []
+                for threshold in thresholds:
+                    collate_fn = partial(collate_threshold,
+                                        tokenizer=tokenizer,
+                                        device=device,
+                                        return_attention_masks=False,
+                                        pad_to_max_length=False,
+                                        threshold=threshold,
+                                        collate_orig=coll_call,
+                                        n_classes=3)
+
+                    saliency_path_test = os.path.join(test_saliency_dir,
+                                                    f'{model_path}_{saliency}')
+
+                    test = NLIDataset(args["dataset_dir"],type="test")
+                    test = DatasetSaliency(test, saliency_path_test)
+
+                    test_dl = BucketBatchSampler(batch_size=model_args["batch_size"],
+                                                dataset=test,
+                                                collate_fn=collate_fn)
+
+                    results = eval_fn(model, test_dl, model_args["labels"])
+                    model_scores.append(results[2])
+
+                print(thresholds, model_scores)
+                aucs.append(auc(thresholds, model_scores))
+
+            print(f'{np.mean(aucs):.2f} ($\pm${np.std(aucs):.2f})')
---
+++ saliency_eval/human_agreement.py
@@ -0,0 +1,74 @@
+"""Computing Human Agreement measure."""
+import argparse
+import json
+
+import numpy as np
+from sklearn.metrics import average_precision_score
+from transformers import BertTokenizer
+from models.data_loader import NLIDataset
+from models.saliency_utils import get_gold_saliency_esnli
+
+if __name__ == "__main__":
+    args= {
+        "subset": "all",
+        "dataset": "snli",
+        "dataset_dir": "data/e-SNLI/dataset",
+        "saliency_path": ["data/saliency/snli/cnn/cnn","data/saliency/snli/random_cnn/cnn"],
+        "saliencies": ["shap", "sal_mean", "sal_l2", "occlusion_none", "lime", "inputx_mean", "inputx_l2", "guided_mean", "guided_l2"]
+    }
+    print(args, flush=True)
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    test = test =NLIDataset(args["dataset_dir"], type='test',salient_features=True)
+
+    for saliency_name in args["saliencies"]:
+        avg_seeds = []
+        for m in range(1,6):
+            for sp in args["saliency_path"]:
+                saliency_path = sp+f"{m}"
+                avgp = []
+
+                prediction_path = saliency_path.replace('saliency',
+                                                        'models') + '.predictions'
+                predictions = json.load(open(prediction_path))['class']
+                saliency_path = saliency_path + '_' + saliency_name
+                with open(saliency_path) as out:
+                    for i, line in enumerate(out):
+                        try:
+                            instance_saliency = json.loads(line)
+                        except:
+                            line = next(out)
+                            instance_saliency = json.loads(line)
+                        saliency = instance_saliency['tokens']
+
+                        instance = test[i]
+                        instance_gold = instance[2]
+                        predicted = predictions[i]
+
+                        token_ids = tokenizer.encode(instance[0], instance[1])
+
+                        token_pred_saliency = []
+                        for record in saliency:
+                            token_pred_saliency.append(record[str(instance_gold)])
+
+                        gold_saliency = get_gold_saliency_esnli(instance,
+                                                        tokenizer.convert_ids_to_tokens(
+                                                            token_ids),
+                                                        [tokenizer.cls_token,
+                                                        tokenizer.sep_token,
+                                                        tokenizer.pad_token],
+                                                        tokenizer)
+
+                        gold_saliency = gold_saliency[:509]
+                        token_pred_saliency = token_pred_saliency[
+                                            :len(gold_saliency)]
+
+                        avgp.append(average_precision_score(gold_saliency,
+                                                            token_pred_saliency))
+
+                    print(len(avgp), np.mean(avgp), flush=True)
+                    avg_seeds.append(np.mean(avgp))
+            print(saliency_name, flush=True)
+            print(f'{np.mean(avg_seeds):.3f} ($\pm${np.std(avg_seeds):.3f})',
+                flush=True)
